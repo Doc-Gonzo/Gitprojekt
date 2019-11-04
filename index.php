0120f5be7bc7926c72893ecd2978d7d09e3a16e5
Mathias Schaller <stoertebecker@gmx.at>
2019-11-04 09:03:29 +0100

    test

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 
 <html>
 <head>
-<title>
-Eine Kleine Testseite.
-</title>
+    <title>Eine Kleine Testseite.</title>
+    <link type="text/css" rel="stylesheet" href="main.css">
 </head>
 <body>
-<h1>GIT-Test</h1>
-<p>Die erste Version unserer Seite.</p>
+    <h1>GIT-Test</h1>
+    <p>Die erste Version unserer Seite.</p>
 </body>
 </html>
